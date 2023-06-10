94654e1bbeffaf43fb9bd573e4edc68bd72c5584
Dominik Jóźwicki <djozwicki@grupazpr.pl>
2023-06-10 17:49:36 +0200

    Add notification

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -13,6 +13,21 @@ onMessage(messaging, async (payload) => {
   const reg = await navigator.serviceWorker.ready;
 
   if (payload.data) {
+    new Notification(payload.data.type, {
+      tag: "FRIEND_REQUEST",
+      body: `${payload.data.name} ${payload.data.message}`,
+      actions: [
+        {
+          action: "decline",
+          title: "Decline",
+        },
+        {
+          action: "accept",
+          title: "Accept",
+        },
+      ],
+    });
+
     reg.showNotification(payload.data.type, {
       tag: "FRIEND_REQUEST",
       body: `${payload.data.name} ${payload.data.message}`,
